d471f635099cf6f427625cab532cdd073398c81d
Valentina <vmilitaru28@gmail.com>
2021-11-11 13:30:26 +0000

    moves the nav bar at the bottom on the original census atlas screen

--- src/Panel.svelte
+++ src/Panel.svelte
@@ -4,21 +4,24 @@
 
 <style>
   main {
+    background: #fff;
     box-sizing: border-box;
     padding: 24px;
     z-index: 2;
     width: 40%;
-    position: absolute;
+    position: relative;
   }
   @media (max-width: 900px) {
     main {
       width: 50%;
+      z-index: 0;
     }
   }
   @media (max-width: 670px) {
     main {
       width: 100%;
       top: 70%;
+      z-index: 0;
     }
   }
 </style>
