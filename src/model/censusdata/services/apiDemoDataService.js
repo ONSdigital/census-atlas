8cc4f7311f0749d20fd612eff731cfaf8cec2d9b
Vivian Allen <vivian.allen@methods.co.uk>
2021-12-07 11:58:13 +0000

    WIP apiDemoDataService ready for testing...?

--- src/model/censusdata/services/apiDemoDataService.js
+++ src/model/censusdata/services/apiDemoDataService.js
@@ -44,43 +44,96 @@ export default class APIDemoDataService {
     return simpleTopicTableCategoryData
   }
 
-  // need to either interrogate map here for coords or have them passed in as args
+  // fetch the ew, lad and lsoa data
   async fetchLsoaCategoryData(categoryId) {
     this.currentCategoryID = categoryId;
 
-    // assemble url with K04000001 and bbox
-    let bboxQuery = [
+    // bbox query
+    const bboxQuery = [
       this.mapZoomBBox.swCorner.lon,
       this.mapZoomBBox.swCorner.lat,
       this.mapZoomBBox.neCorner.lon,
       this.mapZoomBBox.neCorner.lat
     ].join((","));
-    let rowQuery = "K04000001";
-    let colsQuery = categoryId; // need to add some stuff here - totals column, geocode column
-    let geoTypeQuery = this.mapZoomBBox.zoom >= 9 ? "lad,lsoa" : "lad";
-     
+    
+    // additional geography_code row query (NB K04000001 = 'England and Wales')
+    const rowQuery = "K04000001";
+
+    // cols query, including the geography type and geography_code, category totals and category data
+    const dbColumn = this.categoryIDToDBTotalsColumn(categoryId);
+    const dbtotalsColumn = this.categoryIDToDBColumn(categoryId);
+    const colsQuery = [
+      "geo_type",
+      "geography_code",
+      dbtotalsColumn,
+      dbColumn,
+    ].join(",");
+    
     // ask for LAD & LSOA if zoom level > 9, otherwise LAD only
-    let baseURL = "https://5laefo1cxd.execute-api.eu-central-1.amazonaws.com/dev/hello/skinny";
-    let url = `${baseURL}?bbox=${bboxQuery}&rows=${rowQuery}&cols=${colsQuery}&geo_type=${geoTypeQuery}`;
+    const geoTypeQuery = this.mapZoomBBox.zoom >= 9 ? "EW,LAD,LSOA" : "EW,LAD";
+    
+    // assemble URL and query API
+    const baseURL = "https://5laefo1cxd.execute-api.eu-central-1.amazonaws.com/dev/hello/skinny";
+    const url = `${baseURL}?bbox=${bboxQuery}&rows=${rowQuery}&cols=${colsQuery}&geo_type=${geoTypeQuery}`;
     let response = await fetch(url);
     let string = await response.text();
-    let category = this._getCategory(categoryId);
 
-    this.reset();
-    this.dataset.lsoa.data = csvParse(string, (d, index) => {
-      return {
-        code: d["GEOGRAPHY_CODE"],
-        value: +d[category.cell],
-        count: +d[0],
-        perc: (+d[category.cell] / +d[0]) * 100,
+    // parse csv string to dataset
+    let lsoaPercs = [];
+    csvParse(string, (row) => {
+      switch (row["geo_type"]) {
+        case "EW":
+          Object.assign(
+            this.dataset,
+            {
+              englandAndWales: {
+                value: +row[dbColumn],
+                count: +row[dbtotalsColumn],
+                perc: (+row[dbColumn] / +row[dbtotalsColumn]) * 100,
+              }
+            }
+          );
+          break;
+        case "LAD":
+          Object.assign(
+            this.dataset.lad.index,
+            {
+              [row["geography_code"]]: {
+                value: +row[dbColumn],
+                count: +row[dbtotalsColumn],
+                perc: (+row[dbColumn] / +row[dbtotalsColumn]) * 100,
+              }
+            }
+          );
+          break;
+        case "LSOA":
+          const perc = (+row[dbColumn] / +row[dbtotalsColumn]) * 100;
+          lsoaPercs.append(perc);
+          Object.assign(
+            this.dataset.lsoa.index,
+            {
+              [row["geography_code"]]: {
+                value: +row[dbColumn],
+                count: +row[dbtotalsColumn],
+                perc: perc,
+              }
+            }
+          );
+          break;
       };
     });
 
-    this._processLegacyDataset();
+    // add breaks NB - this SHOULD be based on the full dataset for the country, so its almost certainly nonsense here
+    // ToDo - have the API provide these?
+    let chunks = ckmeans(lsoaPercs, config.ux.legend_sections);
+    this.dataset.lsoa.breaks = this._getBreaks(chunks);
+    
+    // add 'higher' geography
+    this.dataset.higher = this.dataset.lad;
+    this.dataset.higher.index["ENGLAND_AND_WALES"] = this.dataset.englandAndWales;
 
     return this.dataset.lsoa.index;
   }
-
 
   async fetchLegendBreakpoints(categoryId) {
     return this.dataset.lsoa.breaks;
@@ -109,59 +162,25 @@ export default class APIDemoDataService {
 
   // ---------------------------------
 
-  _processLegacyDataset() {
-    this.dataset.lsoa.data.sort((a, b) => a.perc - b.perc);
-
-    let vals = this.dataset.lsoa.data.map((d) => d.perc);
-    let chunks = ckmeans(vals, config.ux.legend_sections);
-    this.dataset.lsoa.breaks = this._getBreaks(chunks);
-
-    for (const lsoa of this.dataset.lsoa.data) {
-      this.dataset.lsoa.index[lsoa.code] = lsoa;
+  _decomposeCategoryId(categoryId) {
+    const digitsSuffix = categoryId.match(/\d+$/)[0];
+    return {
+      digitsSuffix: digitsSuffix, 
+      prefix: categoryId.substring(categoryId.lastIndexOf(digitsSuffix), 0)
     }
+  }
 
-    // aggregate lad data
-    for (const lsoa of this.dataset.lsoa.data) {
-      let ladCode = lsoaLookup[lsoa.code].parent;
-      if (!this.dataset.lad.index[ladCode]) {
-        this.dataset.lad.index[ladCode] = {
-          code: ladCode,
-          value: 0,
-          count: 0,
-        };
-      }
-      this.dataset.lad.index[ladCode].value += lsoa.value;
-      this.dataset.lad.index[ladCode].count += lsoa.count;
-    }
+  // adjust for 1-based (nomis bulk, in the db) vs 0-based (nomis api) categories: QS101EW001 -> QS101EW0002
+  _categoryIDToDBColumn(categoryId) {
+    const categoryIdParts = this.decomposeCategoryId(categoryId);
+    const adjustedSuffix = (parseInt(categoryIdParts.digitsSuffix)+1).toString().padStart(4,"0");
+    return categoryIdParts.prefix + adjustedSuffix 
+  }
 
-    for (const ladCode of Object.keys(this.dataset.lad.index)) {
-      this.dataset.lad.index[ladCode].perc =
-        (this.dataset.lad.index[ladCode].value / this.dataset.lad.index[ladCode].count) * 100;
-      this.dataset.lad.data.push(this.dataset.lad.index[ladCode]);
-    }
-
-    this.dataset.lad.data.sort((a, b) => a.perc - b.perc);
-
-    // aggregate national data
-    for (const lsoa of this.dataset.lsoa.data) {
-      this.dataset.englandAndWales.value += lsoa.value;
-      this.dataset.englandAndWales.count += lsoa.count;
-    }
-    this.dataset.englandAndWales.perc = (this.dataset.englandAndWales.value / this.dataset.englandAndWales.count) * 100;
-
-    //
-    this.dataset.higher = this.dataset.lad;
-    this.dataset.higher.index["ENGLAND_AND_WALES"] = this.dataset.englandAndWales;
-    this.dataset.higher.data.push(this.dataset.englandAndWales);
-
-    this.dataset.englandAndWales = {
-      count: 0,
-      value: 0,
-    };
-    this.dataset.lad = {
-      data: [],
-      index: {},
-    };
+  // get totals column (1-based, in the db) from category ID: QS101EW010 -> QS101EW0001
+  _categoryIDToDBTotalsColumn(categoryId) {
+    const categoryIdParts = this.decomposeCategoryId(categoryId);
+    return categoryIdParts.prefix + "0001" 
   }
 
   _getBreaks(chunks) {
@@ -173,12 +192,5 @@ export default class APIDemoDataService {
 
     breaks.push(chunks[chunks.length - 1][chunks[chunks.length - 1].length - 1]);
     return breaks;
-  }
-
-  _getCategory(categoryCode) {
-    return {
-      code: categoryCode,
-      cell: +categoryCode.slice(7, 10),
-    };
   }
 }
